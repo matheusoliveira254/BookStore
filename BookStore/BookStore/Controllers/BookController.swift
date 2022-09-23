fb4105584895460d1ced2db1e787b2608479ccc9
Matheus Oliveira <matheus.oliveira254@gmail.com>
2022-09-22 18:43:31 -0600

    creation of the CRUD functions

---
+++ BookStore/BookStore/Controllers/BookController.swift
@@ -0,0 +1,36 @@
+//
+//  BookController.swift
+//  BookStore
+//
+//  Created by Matheus Oliveira on 9/22/22.
+//
+
+import Foundation
+
+class BookController {
+    //MARK: - Singleton
+    static let sharedInstance = BookController()
+    //MARK: - Source of truth
+    var book: [Book] = []
+    
+    //MARK: - CRUD
+    
+    func create(author: String, title: String, rating: Double, synopsis: String) {
+        let books = Book(author: author, title: title, rating: rating, synopsis: synopsis)
+        book.append(books)
+    }
+    
+    func update(booktToUpdate: Book, newAuthor: String, newTitle: String, newRating: Double, newSynopsis: String) {
+        booktToUpdate.author = newAuthor
+        booktToUpdate.title = newTitle
+        booktToUpdate.rating = newRating
+        booktToUpdate.synopsis = newSynopsis
+        booktToUpdate.lastUpdated = Date()
+    }
+    
+    func delete(bookToDelete: Book) {
+        guard let indexOfBookToBeDeleted = book.firstIndex(of: bookToDelete) else {return}
+        book.remove(at: indexOfBookToBeDeleted)
+        
+    }
+} // End of Class
--- BookStore/BookStore/Model/Book.swift
+++ BookStore/BookStore/Model/Book.swift
@@ -9,16 +9,25 @@ import UIKit
 
 class Book {
     var author: String
-    var tile: String
+    var title: String
     var lastUpdated: Date
     var rating: Double
     var synopsis: String
     
     init(author: String, title: String, lastUpdated: Date = Date(), rating: Double, synopsis: String) {
         self.author = author
-        self.tile = title
+        self.title = title
         self.lastUpdated = lastUpdated
         self.rating = rating
         self.synopsis = synopsis
+    }
+}
+
+extension Book: Equatable {
+    static func == (lhs: Book, rhs: Book) -> Bool {
+        return lhs.title == rhs.title &&
+        lhs.author == rhs.author &&
+        lhs.rating == rhs.rating &&
+        lhs.synopsis == rhs.synopsis
     }
 }
